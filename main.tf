97f32649ba6cf603e4d9a0f1a0d58d25e441d672
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2025-02-21 14:06:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,12 @@
-# Provider Configuration - AWS
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Change to your preferred region
 }
 
-# Define Variables
-variable "instance_type" {
-  description = "EC2 Instance Type"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-  default     = "ami-0c55b159cbfafe1f0"
-}
-
-# Create an EC2 Instance
 resource "aws_instance" "app_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (Replace if needed)
+  instance_type = "t2.micro"
 
   tags = {
     Name = "AppServer"
   }
-}
-
-# Output the Public IP of the EC2 Instance
-output "instance_public_ip" {
-  description = "Public IP of the deployed EC2 instance"
-  value       = aws_instance.app_server.public_ip
 }
